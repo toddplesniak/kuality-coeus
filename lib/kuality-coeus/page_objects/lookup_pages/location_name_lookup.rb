079ae2f7cc9ede4ca27149a7523e5cd8036eb6ec
Todd Plesniak <tplesniak@kuali.co>
2015-06-18 07:36:33 -0700

    IACUC noko select list speed up.

--- lib/kuality-coeus/page_objects/lookup_pages/location_name_lookup.rb
+++ lib/kuality-coeus/page_objects/lookup_pages/location_name_lookup.rb
@@ -7,4 +7,6 @@ class LocationNameLookup < Lookups
   element(:location_name_code) { |b| b.frm.text_field(name: 'locationId') }
   element(:location_name) { |b| b.frm.text_field(name: 'locationName') }
 
+  value(:result_table_text) { |b| b.frm.tbody.text}
+  action(:delete_location) { |name_code, b| b.frm.link(title: "delete Location Name withLocation Name Code=#{name_code} ").click }
 end
--- lib/kuality-coeus/page_objects/lookup_pages/location_type_lookup.rb
+++ lib/kuality-coeus/page_objects/lookup_pages/location_type_lookup.rb
@@ -7,4 +7,7 @@ class LocationTypeLookup < Lookups
   element(:location_type_code) { |b| b.frm.text_field(name: 'locationTypeCode') }
   element(:location_type) { |b| b.frm.text_field(name: 'location') }
 
+  value(:result_table_text) { |b| b.frm.tbody.text}
+  action(:delete_location) { |name_code, b| b.frm.link(title: "delete Location Type withLocation Type Code=#{name_code} ").click }
+
 end
